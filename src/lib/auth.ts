892619df0823aea09a5ab36185575da5ba5f3d5b
adelyaou <octavianiadelya@gmail.com>
2025-08-22 10:21:38 +0700

    WIP | Security set up in compose

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs'
 
 export async function hashPassword(password: string): Promise<string> {
+    const salt = await bcrypt.genSalt(12)
     return await bcrypt.hash(password, 12)
 }
 
